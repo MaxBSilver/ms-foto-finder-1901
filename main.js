54d2f9a18655addceeca1343c02f485e5175366c
Max Silver <Maxbsilver@gmail.com>
2019-02-23 11:35:47 -0700

    Add two load functions

--- main.js
+++ main.js
@@ -5,22 +5,24 @@ var addToAlbumEl = document.querySelector('.add-to-album-btn');
 var captionInputEl = document.querySelector('#caption-input');
 var chooseFileBtnEl = document.querySelector('.choose-file-btn');
 var favBtnEl = document.querySelector('.favorite-btn');
-var images = JSON.parse(localStorage.getItem('images')) || [];
+var viewFavoritesBtnEl = document.querySelector('.view-favorites-btn');
+var searchInputEl = document.querySelector('#search');
+var searchBtnEl = document.querySelector('.search-btn')
 var mainEl = document.querySelector('main');
+var images;
 // var reader = new FileReader();
 
 /* Event Listeners */
-window.addEventListener('load', loadCards);
+window.addEventListener('load', loadFromStorage);
 addToAlbumEl.addEventListener('click', addToAlbum);
+searchBtnEl.addEventListener('click', searchCards);
+viewFavoritesBtnEl.addEventListener('click', viewFavorites);
 // favoriteBtnEl.addEventListener('click', favoriteCards);
 mainEl.addEventListener('click', buttonListener)
 
 /* Functions */
 
-function addToAlbum (event) {
-  event.preventDefault();
-  create();
-}
+/* Create Card */
 function create() {
    var cardId = Date.now();
    var titleInputVal = titleInputEl.value;
@@ -51,12 +53,37 @@ function generateCard(id, title, caption) {
     mainEl.insertAdjacentHTML('afterbegin', card);
     clearInputs();
 }
+
 function clearInputs() {
   titleInputEl.value = '';
   captionInputEl.value = '';
 }
 
-function loadCards(e) {
+/* -- load cards -- */
+
+function loadFromNew(images) {
+  images;
+  if(images.length == 0){
+    return false;
+  }
+    else {
+    i = 0;
+    images.forEach(function(){
+    displayCards(images)
+    })
+  }
+}
+function displayCards(images) {
+  generateCard(images[i].id, images[i].title, images[i].caption);
+  if(images[i].favorited === true){ 
+  var fav = document.querySelector('.favorite-btn');
+  fav.classList.add('red-background');
+  }
+  i++;
+}
+
+function loadFromStorage(e) {
+  images = JSON.parse(localStorage.getItem('images')) || [];
   if(images.length == 0){
     return false;
   }
@@ -64,15 +91,13 @@ function loadCards(e) {
     i = 0;
     images = JSON.parse(localStorage.images);
     images.forEach(function(){
-    generateCard(images[i].id, images[i].title, images[i].caption);
-    if(images[i].favorited === true){ 
-    var fav = document.querySelector('.favorite-btn');
-    fav.classList.add('red-background');
-    }
+    displayCards(images);
+    })
     i++;
-  })
   }
 }
+
+/* -- button functions -- */
 
 function buttonListener(e) {
   var favoriteBtnEl = document.querySelector('#favorite-btn');
@@ -82,8 +107,11 @@ function buttonListener(e) {
     favoriteCards(e);
   } 
 }
-    // e.target.classList.add('red-background');
 
+function addToAlbum (event) {
+  event.preventDefault();
+  create();
+}
 
 function favoriteCards(e) {
   var i = images.indexOf(targetImage[0]);
@@ -100,7 +128,34 @@ function favoriteCards(e) {
   newPhoto.saveToStorage();
 }
 
-  
+function viewFavorites(event) {
+  event.preventDefault();
+  images = JSON.parse(localStorage.getItem('images')) || [];
+  var favoriteList = [];
+  images.forEach(image => {
+    if(image.favorited == true)
+      favoriteList.push(image);
+  })
+  mainEl.innerHTML ='';
+  loadFromNew(favoriteList);
+}
+
+function searchCards() {
+  images = JSON.parse(localStorage.getItem('images')) || [];
+  var searchInputVal = searchInputEl.value;
+  var searchQuery = searchInputVal.toLowerCase();
+  var searchResults = [];
+  images.forEach(image => {
+    if(image.title.toLowerCase().includes(searchQuery) || image.caption.toLowerCase().includes(searchQuery)){
+      searchResults.push(image)
+    }
+      mainEl.innerHTML ='';
+      loadFromNew(searchResults);
+  })
+}
+
+
+/* -- Targeting Functions -- */
 
 function photoTargeter(e) {
   images = JSON.parse(localStorage.getItem('images'))
