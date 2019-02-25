c2101b2a2247364e7318aa8a71a8cdc7f83fbba4
Max Silver <Maxbsilver@gmail.com>
2019-02-24 17:39:46 -0700

    Add content editable

--- main.js
+++ main.js
@@ -1,34 +1,41 @@
 /* Global Variables */
 
-var titleInputEl = document.querySelector('#title-input');
 var addToAlbumEl = document.querySelector('.add-to-album-btn');
 var captionInputEl = document.querySelector('#caption-input');
 var chooseFileBtnEl = document.querySelector('.choose-file-btn');
-var favBtnEl = document.querySelector('.favorite-btn');
-var viewFavoritesBtnEl = document.querySelector('.view-favorites-btn');
-var searchInputEl = document.querySelector('#search');
-var searchBtnEl = document.querySelector('.search-btn');
 var emptyEl = document.querySelector('.empty')
-var mainEl = document.querySelector('main');
-var showMoreBtnEl = document.querySelector('.show-more-btn');
+var favBtnEl = document.querySelector('.favorite-btn');
 var fileInputEl = document.querySelector('.file-input');
-var photoImgEl = document.querySelector('photo-img');
 var images;
+var mainEl = document.querySelector('main');
+var photoImgEl = document.querySelector('photo-img');
 var reader = new FileReader();
-var savedPhoto;
+var searchBtnEl = document.querySelector('.search-btn');
+var searchInputEl = document.querySelector('#search');
+var showMoreBtnEl = document.querySelector('.show-more-btn');
+var titleInputEl = document.querySelector('#title-input');
+var viewFavoritesBtnEl = document.querySelector('.view-favorites-btn');
 
 // var reader = new FileReader();
 
 /* Event Listeners */
-window.addEventListener('load', loadFromStorage);
 addToAlbumEl.addEventListener('click', addToAlbum);
+fileInputEl.addEventListener('change', chooseFile);
+mainEl.addEventListener('click', buttonListener);
+document.addEventListener('click', editCard)
 searchBtnEl.addEventListener('click', searchCards);
 searchInputEl.addEventListener('keyup', searchChecker);
-viewFavoritesBtnEl.addEventListener('click', viewFavoriteToggle);
-mainEl.addEventListener('click', buttonListener);
 showMoreBtnEl.addEventListener('click', showMore);
-fileInputEl.addEventListener('change', chooseFile);
+viewFavoritesBtnEl.addEventListener('click', viewFavoriteToggle);
+window.addEventListener('load', loadFromStorage);
+// document.addEventListener('click', editCard)
+window.addEventListener('keypress', function (e) {
+    if (e.keyCode === 13) {
+        editCard(e);
+        e.target.setAttribute("contentEditable", false);
 
+    }
+  }) 
 
 /* Functions */
 
@@ -50,8 +57,8 @@ function addPhoto(e) {
  return e.target.result;
 }
 
-
 /* Create Card */
+
 function create(e) {
    var cardId = Date.now();
    var titleInputVal = titleInputEl.value;
@@ -64,10 +71,7 @@ function create(e) {
     newPhoto.saveToStorage();
 }
 
-
-//remove global variable for savedIdeas
 function displayCreate(images){
-  console.log(captionInputEl.value)
    if(captionInputEl.value && titleInputEl.value != ''){
     i = images.length - 1;
     generateCard(images[i].id, images[i].title, images[i].caption, images[i].file);
@@ -78,13 +82,13 @@ function generateCard(id, title, caption, file) {
   event.preventDefault();
   var card = `<article class="photo" data-id=${id}>
       <section class="photo-title">
-        <h2>${title}</h2>
+        <h2 contentEditable="true">${title}</h2>
       </section>
       <section class="photo-img-wrapper">
         <img class ="photo-img" src='${file}'>
       </section>
       <section class="photo-caption">
-        <p>${caption}</p>
+        <p contentEditable="true">${caption}</p>
       </section>
       <section class="photo-btns">
         <button id="trash-btn" class="trash-btn"></button>
@@ -92,7 +96,6 @@ function generateCard(id, title, caption, file) {
       </section>
     </article>`
     mainEl.insertAdjacentHTML('afterbegin', card);
-    clearInputs();
 }
 
 function clearInputs() {
@@ -120,6 +123,25 @@ function loadFromNew(images) {
     }
   }
 
+function loadFromStorage(e) {
+  images = JSON.parse(localStorage.getItem('images')) || [];
+  emptyMessage(images);
+  i = 0;
+  if(images.length != 0 && images.length > 10){
+    let arrLength = parseInt(images.length);
+    let arrLengthMax = parseInt(images.length - 10);
+    images = images.slice(arrLengthMax, arrLength);
+    images.forEach(function(){
+    displayCards(images);
+    })
+  }
+    else if(images.length !=0 && images.length <  10){
+      images.forEach(function(){
+      displayCards(images);
+      })
+    }
+}
+
 function displayCards(images) {
   generateCard(images[i].id, images[i].title, images[i].caption, images[i].file);
   if(images[i].favorited === true){ 
@@ -138,21 +160,6 @@ function emptyMessage(images){
   }
 }
 
-function loadFromStorage(e) {
-  images = JSON.parse(localStorage.getItem('images')) || [];
-  emptyMessage(images);
-  if(images.length != 0){
-    i = 0;
-    images = JSON.parse(localStorage.images);
-    let arrLength = parseInt(images.length);
-    let arrLengthMax = parseInt(images.length - 10);
-    images = images.slice(arrLengthMax, arrLength);
-    images.forEach(function(){
-      displayCards(images);
-    })
-  }
-}
-
 
 /* -- button functions -- */
 
@@ -167,8 +174,14 @@ function buttonListener(e) {
   else if(e.target.id == trashBtnEl.id){
     photoTargeter(e);
     deleteCards(e);
-    
   }
+
+}
+
+function addToAlbum (event) {
+  event.preventDefault();
+  displayCreate(images);
+  emptyMessage(images);
 }
 
 function showMore() {
@@ -187,11 +200,34 @@ function showMore() {
   }
 }
 
-function addToAlbum (event) {
-  event.preventDefault();
-  displayCreate(images);
-  emptyMessage(images);
+function searchChecker() {
+  var favoritedImages = [];
+  if (viewFavoritesBtnEl.innerText === 'View Favorites') {
+    searchCards(images)
+  }
+  else {
+    images.forEach(image => {
+      if(image.favorited === true){
+      favoritedImages.push(image);
+    }
+    })
+      searchCards(favoritedImages);
+  }
 }
+
+function searchCards(images) {
+  var searchInputVal = searchInputEl.value;
+  var searchQuery = searchInputVal.toLowerCase();
+  var searchResults = [];
+  images.forEach(image => {
+    if(image.title.toLowerCase().includes(searchQuery) || image.caption.toLowerCase().includes(searchQuery)){
+      searchResults.push(image)
+    }
+      mainEl.innerHTML ='';
+      loadFromNew(searchResults);
+  })
+}
+/* -- Card Functions --*/
 
 function deleteCards(e) { 
   var i = images.indexOf(targetImage[0]);
@@ -242,34 +278,32 @@ function viewFavorites(event) {
   loadFromNew(favoriteList);
 }
 
-function searchChecker() {
-  var favoritedImages = [];
-  if (viewFavoritesBtnEl.innerText === 'View Favorites') {
-    searchCards(images)
+
+/* -- Editing Card Functions -- */ 
+function editCard(e) {
+  photoTargeter(e);
+  var newPhoto = new Photo(targetImage[0].id, targetImage[0].title, targetImage[0].caption, targetImage[0].file, targetImage[0].favorited);
+  i = images.indexOf(targetImage[0]);
+  if (e.target == null || undefined){
+    newPhoto.title = e.target.innerHTML;
+    newPhoto.caption = e.target.innerHTML;
   }
-  else {
-    images.forEach(image => {
-      if(image.favorited === true){
-      favoritedImages.push(image);
-    }
-    })
-      searchCards(favoritedImages);
+  if(e.target.tagName === 'H2'){
+  e.target.setAttribute("contentEditable", true);
+  newPhoto.title = e.target.innerHTML;
+ }
+  else if(e.target.tagName === 'P'){
+  e.target.setAttribute("contentEditable", true);
+  newPhoto.caption = e.target.innerHTML;
   }
+  updateAndSaveToStorage(i, newPhoto);
 }
 
-function searchCards(images) {
-  var searchInputVal = searchInputEl.value;
-  var searchQuery = searchInputVal.toLowerCase();
-  var searchResults = [];
-  images.forEach(image => {
-    if(image.title.toLowerCase().includes(searchQuery) || image.caption.toLowerCase().includes(searchQuery)){
-      searchResults.push(image)
-    }
-      mainEl.innerHTML ='';
-      loadFromNew(searchResults);
-  })
+function updateAndSaveToStorage(i, newPhoto){
+ images.splice(i, 1, newPhoto);
+ newPhoto.updateStorage(i, newPhoto);
+ newPhoto.saveToStorage;
 }
-
 
 /* -- Targeting Functions -- */
 
@@ -281,25 +315,3 @@ function photoTargeter(e) {
   })
 }
 
-
-// function appendPhotos() {
-//   imagesArr.forEach(function (photo) {
-//   photoGallery.innerHTML += `<img src=${photo.file} />`
-//   })
-// }
-
-// function loadImg() {
-//   console.log(input.files[0])
-//   if (input.files[0]) {
-//     reader.readAsDataURL(input.files[0]); 
-//     reader.onload = addPhoto
-//   }
-// }
-
-// function addPhoto(e) {
-//   console.log(e.target.result);
-//   var newPhoto = new Photo(Date.now(), e.target.result);
-//   photoGallery.innerHTML += `<img src=${e.target.result} />`;
-//   imagesArr.push(newPhoto);
-//   newPhoto.saveToStorage(imagesArr);
-// }
